ffb55fc2de004c8f354a3ed83b2ef9bb5b98ad18
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-04-04 01:51:33 +0530

    routes

---
+++ TODO-Backend/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const route = express.Router();
+const TodoControllers = require("../controllers/todocontroller")
+
+
+route.get("/",TodoControllers.getTodos);
+route.post("/",TodoControllers.createTodo);
+route.put("/:id",TodoControllers.updateTodo);
+route.delete("/:id",TodoControllers.deleteTodo);
+
+
+
+module.exports = route;
